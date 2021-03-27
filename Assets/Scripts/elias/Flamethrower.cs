aa28bfb349e4c0879a6a77e7d7efcf4c526c2b9b
elias-axelsson <eliasaxelsson@hotmail.com>
2021-03-28 00:23:07 +0100

    Flamethrower
    Creation of flamethrower and fire effect
    Creation of v1 of flamethrower script
    Moved my own scripts into a folder to keep track on the files I am working on

---
+++ Assets/Scripts/elias/Flamethrower.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flamethrower : MonoBehaviour
+{
+    public GameObject flameEffect;
+
+    Vector3 position;
+    Camera mainCam;
+
+    void Start()
+    {
+        mainCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position = transform.position;
+        if (Input.GetMouseButtonDown(0))
+        {
+            Fire();
+        }
+    }
+    void Fire()
+    {
+        Vector3 direction = mainCam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        direction.Normalize();
+        GameObject flame = Instantiate(flameEffect, transform.position, transform.rotation);
+    }
+}
