c271f2c7a99a5734bbf7e45b9f177c7ee8198a53
elias-axelsson <eliasaxelsson@hotmail.com>
2021-03-28 23:40:29 +0200

    bruh moment
    försökte göra fler grejer med flamethrower och shotgun, fortsätter imorn

--- Assets/Scripts/elias/Flamethrower.cs
+++ Assets/Scripts/elias/Flamethrower.cs
@@ -5,28 +5,46 @@ using UnityEngine;
 public class Flamethrower : MonoBehaviour
 {
     public GameObject flameEffect;
-
+    //public ParticleSystem flame2;
     Vector3 position;
     Camera mainCam;
 
     void Start()
     {
         mainCam = Camera.main;
+
+        //flame2.Pause();
     }
 
     // Update is called once per frame
     void Update()
     {
         position = transform.position;
+        
+        //flame2.transform.position = position;
+        //flame2.transform.rotation = Quaternion.Euler(0, 0, angle);
+        //flameEffect.transform.position = transform.position;
+        //flameEffect.transform.rotation = transform.rotation;
         if (Input.GetMouseButtonDown(0))
         {
             Fire();
         }
+        //else
+        //    flame2.Pause();
     }
     void Fire()
     {
         Vector3 direction = mainCam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         direction.Normalize();
-        GameObject flame = Instantiate(flameEffect, transform.position, transform.rotation);
+        //flame2.Play();
+        //flameEffect.SetActive(true);
+        //bb
+        Vector3 mouse = Input.mousePosition; //where the mouse is
+        Vector3 screenPoint = mainCam.WorldToScreenPoint(transform.localPosition); //player position in screen space
+
+        Vector2 distance = new Vector2(mouse.x - screenPoint.x, mouse.y - screenPoint.y); //distance between mouse and player
+        float angle = Mathf.Atan2(distance.y, distance.x) * Mathf.Rad2Deg; //the angle, in degrees
+        //aa
+        GameObject flame = Instantiate(flameEffect, position, Quaternion.Euler(0, angle, 0));
     }
 }
---
+++ Assets/Scripts/elias/Shotgun.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shotgun : MonoBehaviour
+{
+    public Transform spawnpoint;
+    public GameObject impact;
+
+    Camera mainCam;
+    float distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        distance = 15f;
+        mainCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Shoot()
+    {
+        RaycastHit2D hit;
+        RaycastHit2D hit2;
+
+        //if(Physics2D.Raycast())
+    }
+}
