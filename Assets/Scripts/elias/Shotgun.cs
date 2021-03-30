09dae74fbbac66c682c94d593e3715271198373d
elias-axelsson <eliasaxelsson@hotmail.com>
2021-03-30 21:52:36 +0200

    bruh
    bruh 2

--- Assets/Scripts/elias/Shotgun.cs
+++ Assets/Scripts/elias/Shotgun.cs
@@ -10,6 +10,8 @@ public class Shotgun : MonoBehaviour
     Camera mainCam;
     float distance;
 
+    Vector3 direction;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,13 +22,26 @@ public class Shotgun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        direction = mainCam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        direction.Normalize();
+        if (Input.GetMouseButton(0))
+        {
+            Shoot();
+        }
     }
     void Shoot()
     {
-        RaycastHit2D hit;
-        RaycastHit2D hit2;
-
-        //if(Physics2D.Raycast())
+        RaycastHit2D hit=Physics2D.Raycast(transform.position, direction, distance);
+        if (hit)
+        {
+            Debug.Log("Bruh mannen");
+        }
+        //Debug.DrawRay(transform.position, direction, Color.red);
+        //RaycastHit2D hit2;
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawRay(transform.position, Input.mousePosition);
     }
 }
