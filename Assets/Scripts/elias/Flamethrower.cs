af616b06dc8de68c1e24ac771739774f63d59ae0
elias-axelsson <eliasaxelsson@hotmail.com>
2021-03-31 20:56:46 +0200

    flamethrower n shit

--- Assets/Scripts/elias/Flamethrower.cs
+++ Assets/Scripts/elias/Flamethrower.cs
@@ -4,47 +4,53 @@ using UnityEngine;
 
 public class Flamethrower : MonoBehaviour
 {
-    public GameObject flameEffect;
-    //public ParticleSystem flame2;
+    public ParticleSystem flameEffect;
+    public Transform flameStartPos;
+
     Vector3 position;
     Camera mainCam;
-
+    GameObject flame;
+    //ParticleSystemRenderer partRend;
     void Start()
     {
         mainCam = Camera.main;
-
+        //flameStartPos = new Vector3(transform.position.x + 0.46f, transform.position.y, transform.position.z);
         //flame2.Pause();
+        //partRend = flameEffect.GetComponent<ParticleSystemRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         position = transform.position;
-        
         //flame2.transform.position = position;
         //flame2.transform.rotation = Quaternion.Euler(0, 0, angle);
         //flameEffect.transform.position = transform.position;
         //flameEffect.transform.rotation = transform.rotation;
         if (Input.GetMouseButtonDown(0))
         {
+            //partRend.enabled = true;
             Fire();
         }
-        //else
-        //    flame2.Pause();
+        //else if (Input.GetMouseButtonUp(0))
+        //{
+        //    flameEffect.Stop(true);
+        //}
+
     }
     void Fire()
     {
-        Vector3 direction = mainCam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        direction.Normalize();
+        //Vector3 direction = mainCam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        //direction.Normalize();
         //flame2.Play();
         //flameEffect.SetActive(true);
         //bb
-        Vector3 mouse = Input.mousePosition; //where the mouse is
-        Vector3 screenPoint = mainCam.WorldToScreenPoint(transform.localPosition); //player position in screen space
-
-        Vector2 distance = new Vector2(mouse.x - screenPoint.x, mouse.y - screenPoint.y); //distance between mouse and player
-        float angle = Mathf.Atan2(distance.y, distance.x) * Mathf.Rad2Deg; //the angle, in degrees
-        //aa
-        GameObject flame = Instantiate(flameEffect, position, Quaternion.Euler(0, angle, 0));
+        /*Vector3 screenPoint = mainCam.WorldToScreenPoint(transform.localPosition);*/ //player position in screen space
+        Vector3 mouseCameraPos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        //Vector2 distance = Input.mousePosition-screenPoint; //distance between mouse and player
+        //distance.Normalize();
+        //float angle = Mathf.Atan2(distance.y, distance.x) * Mathf.Rad2Deg; //the angle, in degrees
+        flame = Instantiate(flameEffect.gameObject, flameStartPos.position, Quaternion.identity);
+        flame.transform.LookAt(mouseCameraPos);
     }
 }
