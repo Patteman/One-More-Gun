81c9e4ae173d965c5f3a2ce4895953f1ba08b0cc
Patrik Zielinski <patte_patrik@outlook.com>
2021-04-02 15:18:18 +0200

    Implemented projectile / protection physics.
    Bullets and simple covers now get destroyed when they hit each other. Now easily adjustable to how we want it to work. Re-did scripts and such. Organized the project a bit more.

---
+++ Assets/Scripts/StageScripts/ProtectionScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProtectionScript : MonoBehaviour
+{
+    public float coverHealth;
+    public float maxHealth;
+
+
+    void Start()
+    {
+        coverHealth = maxHealth;
+    }
+
+    
+    void Update()
+    {
+
+        if (coverHealth <= 0)
+        {
+            Destroy(gameObject);
+            
+        }
+    }
+
+    void Collision(Collision2D other)
+    {
+        if (other.gameObject.tag == "BasicProjectiles")
+        {
+            coverHealth -= 25;
+        }
+    }
+
+    void OnCollisionEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "BasicProjectiles")
+        {
+            coverHealth -= 25f;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "BasicProjectiles")
+        {
+            coverHealth -= 25f;
+        }
+    }
+}
