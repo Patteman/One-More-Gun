398b6297be8ca446a83f0f25b7f17742e4ac794b
Patrik Zielinski <patte_patrik@outlook.com>
2021-04-29 16:08:36 +0200

    Minor fixes.
    Removed boundries code in Playerscript and added physical boundries instead.
    Adjusted camera settings  for my scene and updated stage1 prefab.

--- Assets/Scripts/PlayerPrototype/PlayerScript.cs
+++ Assets/Scripts/PlayerPrototype/PlayerScript.cs
@@ -37,9 +37,6 @@ public class PlayerScript : MonoBehaviour
 
     void Start()
     {
-
-
-        CalculateScreenSize();
         onSpawn = true;
 
         //inventory = new List<GameObject>();
@@ -100,11 +97,6 @@ public class PlayerScript : MonoBehaviour
         rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
     }
 
-    private void LateUpdate()
-    {
-        UpdateBounds();
-    }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Finish")
@@ -120,20 +112,5 @@ public class PlayerScript : MonoBehaviour
             Debug.Log("You've left the spawn!");
             onSpawn = false;
         }
-    }
-
-    private void CalculateScreenSize()
-    {
-        screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
-        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x;
-        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y;
-    }
-
-    private void UpdateBounds()
-    {
-        Vector3 viewPos = transform.position;
-        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x * -1 + objectWidth, screenBounds.x - objectWidth);
-        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y * -1 + objectHeight, screenBounds.y - objectHeight);
-        transform.position = viewPos;
     }
 }
