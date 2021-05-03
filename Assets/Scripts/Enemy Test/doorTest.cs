6acc606300e059f948704b1f67e30ff1cd9850ab
Kevin Bjørn Roulund Wilken <kwilken1608@hotmail.dk>
2021-05-03 15:52:14 +0200

    door affects navmesh
    Made a small door test. The NavMesh will now detect the door, and when it opens (disables atm) the navmesh will fill in where the door was.

---
+++ Assets/Scripts/Enemy Test/doorTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorTest : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("p"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy Test/objectTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectTest : MonoBehaviour
+{
+    public float speed = 1f;
+    public float countdown = 0.5f;
+    public float deathtimer = 10f;
+
+    Vector3 pos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        deathtimer -= Time.deltaTime;
+        countdown -= Time.deltaTime;
+        pos.y += speed * Time.deltaTime;
+        pos.x = -4.5f;
+        pos.z = -1.5f;
+        if (countdown <= 0f)
+        {
+            speed = speed * -1;
+            countdown = 0.5f;
+        }
+
+        transform.position = pos;
+
+        if (deathtimer <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
