4896c819d6012dbb5026d2fa1d004b4ffb7f9e2a
Kevin Bjørn Roulund Wilken <kwilken1608@hotmail.dk>
2021-05-31 12:35:08 +0200

    Working Keycard
    Made a keycard with a nice sprite, that pulses to signal that it can be interacted with. when the player walks over the keycard, the assigned door will open.

---
+++ Assets/Scripts/Keycard.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keycard : MonoBehaviour
+{
+    public GameObject door;
+    private bool coroutineAllowed;
+
+    private void Start()
+    {
+        coroutineAllowed = true;
+    }
+
+    private void Update()
+    {
+        if (coroutineAllowed)
+        {
+            StartCoroutine("StartPulsing");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            door.SetActive(false);
+            Debug.Log("Keycard picked up");
+            Destroy(gameObject);
+        }
+    }
+
+    private IEnumerator StartPulsing()
+    {
+        coroutineAllowed = false;
+        
+        for (float i = 0f; i <= 1f; i+=0.1f)
+        {
+            transform.localScale = new Vector2(
+            (Mathf.Lerp(transform.localScale.x, transform.localScale.x + 0.025f, Mathf.SmoothStep(0f, 1f, i))),
+            (Mathf.Lerp(transform.localScale.y, transform.localScale.y + 0.025f, Mathf.SmoothStep(0f, 1f, i)))
+            );
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        for (float i = 0f; i <= 1f; i += 0.1f)
+        {
+            transform.localScale = new Vector2(
+            (Mathf.Lerp(transform.localScale.x, transform.localScale.x - 0.025f, Mathf.SmoothStep(0f, 1f, i))),
+            (Mathf.Lerp(transform.localScale.y, transform.localScale.y - 0.025f, Mathf.SmoothStep(0f, 1f, i)))
+            );
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        coroutineAllowed = true;
+    }
+}
