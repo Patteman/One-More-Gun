271a5ca64d8eebacc15da64b30a2787da5d5e71c
Kevin Bjørn Roulund Wilken <kwilken1608@hotmail.dk>
2021-05-31 16:15:45 +0200

    Added comments

--- Assets/Scripts/Keycard.cs
+++ Assets/Scripts/Keycard.cs
@@ -14,12 +14,14 @@ public class Keycard : MonoBehaviour
 
     private void Update()
     {
+        //Checks if the coroutine is already started or not. If it has not started yet, then start coroutine.
         if (coroutineAllowed)
         {
             StartCoroutine("StartPulsing");
         }
     }
 
+    //Checks if player moved over keycard, if so, open the assigned door, and remove the keycard.
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
@@ -30,10 +32,13 @@ public class Keycard : MonoBehaviour
         }
     }
 
+    //Coroutine to make the keycard "pulse" so it is easy to differentiate from the background aesthetics.
     private IEnumerator StartPulsing()
     {
+        //Makes sure the coroutine cannot be started again before the code has been run fully.
         coroutineAllowed = false;
         
+        //Makes the scale increase by 0.025f 10 times, every 0.05f secounds.
         for (float i = 0f; i <= 1f; i+=0.1f)
         {
             transform.localScale = new Vector2(
@@ -43,6 +48,7 @@ public class Keycard : MonoBehaviour
             yield return new WaitForSeconds(0.05f);
         }
 
+        //Makes the scale decrease by 0.025f 10 times, every 0.05f secounds.
         for (float i = 0f; i <= 1f; i += 0.1f)
         {
             transform.localScale = new Vector2(
@@ -52,6 +58,7 @@ public class Keycard : MonoBehaviour
             yield return new WaitForSeconds(0.05f);
         }
 
+        //Coroutine is done, let script know it can be started again.
         coroutineAllowed = true;
     }
 }
--- Assets/Scripts/Medkit.cs
+++ Assets/Scripts/Medkit.cs
@@ -6,8 +6,8 @@ public class Medkit : MonoBehaviour
 {
     public int healAmount = 30;
     public float lifeTime = 10f;
+    
     private float fadePercent;
-
     private bool coroutineAllowed;
     private float despawnTimer;
     private SpriteRenderer spriteRenderer;
@@ -16,10 +16,13 @@ public class Medkit : MonoBehaviour
     {
         coroutineAllowed = true;
         despawnTimer = 0f;
-        fadePercent = lifeTime * 0.7f;
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+
+        //Sets fadePercent to 70% of the lifeTime value.
+        fadePercent = lifeTime * 0.7f;
     }
 
+    //Start counting, check if count is above fade% and if the coroutine is started or not, then start coroutine.
     void Update()
     {
         despawnTimer += Time.deltaTime;
@@ -33,16 +36,21 @@ public class Medkit : MonoBehaviour
         }
     }
 
+    //Makes the medkit "blink" every 0.2 seconds, to signal that the object will soon dissapear.
     private IEnumerator StartFading()
     {
+        //Makes sure the coroutine cannot be started again before the code has been run fully.
         coroutineAllowed = false;
 
+        //Sets the Alpha of the medkit to 0.2f (semi-transparent) then waits 0.2 seconds.
         spriteRenderer.material.color = new Color(1f, 1f, 1f, 0.2f);
         yield return new WaitForSeconds(0.2f);
 
+        //Sets the Alpha of the medkit to 1f (full color) then waits 0.2 seconds.
         spriteRenderer.material.color = new Color(1f, 1f, 1f, 1f);
         yield return new WaitForSeconds(0.2f);
 
+        //Coroutine is done, let script know it can be started again.
         coroutineAllowed = true;
     }
 }
